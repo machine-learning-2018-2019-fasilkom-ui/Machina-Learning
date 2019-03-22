078982d70d73b5a20202088b16629a79039dacbd
Kautsar Fadlillah <kautsarfadlillah@gmail.com>
2019-03-22 15:38:24 +0700

    Add empty class for MNB_TextClassifier

---
+++ MNB_TextClassifier.py
@@ -0,0 +1,23 @@
+class MNB_TextClassifier:
+
+	def __init__(self):
+		self.prior = {}
+		self.condprob = {}
+
+	def __proba_x_given_y(self, x, y):
+		pass
+    
+    def proba_y_given_x(self, y, x):
+        pass
+    
+    def proba_y(self, y):
+        pass
+
+    def fit(self, X, y):
+        pass
+        
+    def predict_single(self, x):
+        pass
+        
+    def predict(self, X):
+        pass
